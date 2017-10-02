e91f73fabf2ab77a566051574803a5f4f11b57aa
Paulo Henrique Alves <paulohenriqueas13@gmail.com>
2017-10-02 16:18:38 -0300

    Add new script.

---
+++ third-scripts/get_image.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/ruby
+
+require 'fog/oraclecloud'
+require 'terminal-table'
+
+## Get image
+def get(name)
+    data = Fog::Compute[:oraclecloud].images.get_public(name)
+end
+
+image = get(ARGV[0])
+rows = []
+rows << ['Name:', image.name]
+rows << :separator
+rows << ['Account:', image.account]
+rows << :separator
+rows << ['Sizes:', image.sizes]
+rows << :separator
+rows << ['Hypervisor:', image.hypervisor]
+rows << :separator
+rows << ['URI:', image.uri]
+rows << :separator
+rows << ['Quota:', image.quota]
+rows << :separator
+rows << ['Platform:', image.platform]
+rows << :separator
+rows << ['No Upload:', image.no_upload]
+rows << :separator
+rows << ['State:', image.state]
+rows << :separator
+rows << ['Signed By:', image.signed_by]
+rows << :separator
+rows << ['File:', image.file]
+rows << :separator
+rows << ['Signature:', image.signature]
+rows << :separator
+rows << ['Checksums:', image.checksums]
+rows << :separator
+rows << ['Error Reason:', image.error_reason]
+rows << :separator
+rows << ['Image Format:', image.image_format]
+rows << :separator
+rows << ['Audited:', image.audited]
+
+table = Terminal::Table.new :rows => rows
+
+puts table
