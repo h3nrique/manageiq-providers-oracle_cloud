bbb87f8caf54127d59fabec195a553e203b6002c
Paulo Henrique Alves <paulohenriqueas13@gmail.com>
2017-10-03 15:19:28 -0300

    Ajustes.

--- app/models/manageiq/providers/oracle_cloud/cloud_manager.rb
+++ app/models/manageiq/providers/oracle_cloud/cloud_manager.rb
@@ -4,13 +4,16 @@ class ManageIQ::Providers::OracleCloud::CloudManager < ManageIQ::Providers::Clou
 
   include ManageIQ::Providers::OracleCloud::ManagerMixin
 
-  alias_attribute :domain, :uid_ems
+  alias_attribute :oracle_cloud_domain, :uid_ems
 
   has_many :resource_groups, :foreign_key => :ems_id, :dependent => :destroy
 
   supports :discovery
   supports :provisioning
   supports :regions
+
+  before_create :ensure_managers
+  before_update :ensure_managers_zone_and_provider_region
 
   def ensure_network_manager
     build_network_manager(:type => 'ManageIQ::Providers::OracleCloud::NetworkManager') unless network_manager
--- app/models/manageiq/providers/oracle_cloud/manager_mixin.rb
+++ app/models/manageiq/providers/oracle_cloud/manager_mixin.rb
@@ -6,20 +6,15 @@ module ManageIQ::Providers::OracleCloud::ManagerMixin
 
     username    = options[:user] || authentication_userid(options[:auth_type])
     password    = options[:pass] || authentication_password(options[:auth_type])
-    domain      = options[:domain]
     region      = options[:region] || provider_region
     compute_api = 'https://compute.uscom-central-1.oraclecloud.com/' # TODO :: Update!!!
     storage_api = '<STORAGE_API>' # TODO :: Update!!!
-    self.class.raw_connect(username, password, domain, region, compute_api, storage_api)
+    self.class.raw_connect(username, password, oracle_cloud_domain, region, compute_api, storage_api)
   end
 
   def verify_credentials(_auth_type = nil, options = {})
     require 'fog/oraclecloud'
     conf = connect(options)
-  rescue ArgumentError => err
-    raise MiqException::MiqInvalidCredentialsError, _("Incorrect credentials - #{err.message}")
-  rescue MiqException::MiqInvalidCredentialsError
-    raise # Raise before falling into catch-all block below
   rescue => err
     _log.error("Error Class=#{err.class.name}, Message=#{err.message}, Backtrace=#{err.backtrace}")
     raise MiqException::MiqInvalidCredentialsError, _("Unexpected response returned from system: #{err.message}")
@@ -28,8 +23,7 @@ module ManageIQ::Providers::OracleCloud::ManagerMixin
   end
 
   module ClassMethods
-    def raw_connect(username, password, domain, region, compute_api = nil, storage_api = nil)
-
+    def raw_connect(username, password, domain, region, compute_api, storage_api)
       require 'fog/oraclecloud'
 
       if domain.blank?
