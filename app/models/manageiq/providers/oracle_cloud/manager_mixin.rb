6e948a65196845437e7eb471fb983d46e4f90254
Paulo Henrique Alves <paulohenriqueas13@gmail.com>
2017-10-03 22:21:31 -0300

    Ajustes.

--- app/models/manageiq/providers/oracle_cloud/manager_mixin.rb
+++ app/models/manageiq/providers/oracle_cloud/manager_mixin.rb
@@ -6,10 +6,7 @@ module ManageIQ::Providers::OracleCloud::ManagerMixin
 
     username    = options[:user] || authentication_userid(options[:auth_type])
     password    = options[:pass] || authentication_password(options[:auth_type])
-    region      = options[:region] || provider_region
-    compute_api = 'https://compute.uscom-central-1.oraclecloud.com/' # TODO :: Update!!!
-    storage_api = '<STORAGE_API>' # TODO :: Update!!!
-    self.class.raw_connect(username, password, oracle_cloud_domain, region, compute_api, storage_api)
+    self.class.raw_connect(username, password, domain, compute_api)
   end
 
   def verify_credentials(_auth_type = nil, options = {})
@@ -23,26 +20,90 @@ module ManageIQ::Providers::OracleCloud::ManagerMixin
   end
 
   module ClassMethods
-    def raw_connect(username, password, domain, region, compute_api, storage_api)
+    def raw_connect(username, password, domain, compute_api)
       require 'fog/oraclecloud'
 
       if domain.blank?
         raise MiqException::MiqInvalidCredentialsError, _("Incorrect domain - check your domain")
       end
 
-      connection = Fog::XML::Connection.new(compute_api)
-      connection.request({
-        :expects => 204,
-        :method  => 'POST',
-        :path    => "/authenticate/",
-        :headers => {
-          'Content-Type' => 'application/oracle-compute-v3+json'
-        },
-        :body    => Fog::JSON.encode({
-           'user' 		 => "/Compute-#{domain}/#{username}",
-           'password' => password
-         })
+      Fog::Compute.new({
+        provider: 'oraclecloud',
+        oracle_username: username,
+        oracle_password: password,
+        oracle_domain: domain,
+        oracle_compute_api: compute_api
       })
+    end
+
+    # Discovery
+    def discover(username, password, domain, compute_api)
+      new_emses         = []
+
+      all_emses         = includes(:authentications)
+      all_ems_names     = all_emses.index_by(&:name)
+
+      known_emses       = all_emses.select { |e| e.authentication_userid == clientid }
+      known_ems_regions = known_emses.index_by(&:provider_region)
+
+      compute           = raw_connect(username, password, domain, compute_api)
+
+#      compute.list_locations.each do |region|
+#        next if known_ems_regions.include?(region.name)
+#        next if vms_in_region(compute, region.name).count == 0 # instances
+        # TODO: Check if images are == 0 and if so then skip
+#        new_emses << create_discovered_region(region.name, clientid, clientkey, azure_tenant_id, subscription, all_ems_names)
+#      end
+
+      # at least create the Azure-eastus region.
+      if new_emses.blank? && known_emses.blank?
+        new_emses << create_discovered_region("eastus", username, password, domain, compute_api, all_ems_names)
+      end
+
+      EmsRefresh.queue_refresh(new_emses) unless new_emses.blank?
+
+      new_emses
+    end
+
+    def discover_queue(username, password, domain, compute_api)
+      MiqQueue.put(
+          :class_name  => name,
+          :method_name => "discover_from_queue",
+          :args        => [username, MiqPassword.encrypt(password), domain, compute_api]
+      )
+    end
+
+    def vms_in_region(compute, region)
+      compute.instances()
+    end
+
+    def discover_from_queue(username, password, domain, compute_api)
+      discover(username, MiqPassword.decrypt(password), domain, compute_api)
+    end
+
+    def create_discovered_region(region_name, username, password, domain, compute_api, all_ems_names)
+
+      name = region_name
+      name = "#{region_name} #{username}" if all_ems_names.include?(name)
+      while all_ems_names.include?(name)
+        name_counter = name_counter.to_i + 1 if defined?(name_counter)
+        name = "#{region_name} #{name_counter}"
+      end
+
+      new_ems = create!(
+          :name            => name,
+          :provider_region => region_name,
+          :zone            => Zone.default_zone,
+          :domain         => domain,
+          :compute_api    => compute_api
+      )
+      new_ems.update_authentication(
+          :default => {
+              :userid   => username,
+              :password => password
+          }
+      )
+      new_ems
     end
   end
 end
